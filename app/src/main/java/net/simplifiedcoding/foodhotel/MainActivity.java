43daae0b131c07893e9882520d7ecad17268fdc0
Eli Kiprop <109068401+elikiprop@users.noreply.github.com>
2024-03-14 12:49:43 +0300

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/foodhotel/MainActivity.java
@@ -0,0 +1,14 @@
+package net.simplifiedcoding.foodhotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
